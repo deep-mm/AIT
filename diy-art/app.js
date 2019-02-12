c2e07297983b8299f326bcaaad3d70385bf247d0
Deep Mehta <deep.mm@somaiya.edu>
2019-02-12 10:49:52 +0530

    Update

--- diy-art/app.js
+++ diy-art/app.js
@@ -1,11 +1,10 @@
 // Full Documentation - https://www.turbo360.co/docs
 const firebase = require("firebase");
-
 require("firebase/auth");
 require("firebase/database");
 require("firebase/firestore");
 
-var config = {
+const config = {
 	apiKey: "AIzaSyBvkSwewV6w7MA86TPMh-Tg0HDk9_TGfiY",
 	authDomain: "diy-market-f233d.firebaseapp.com",
 	databaseURL: "https://diy-market-f233d.firebaseio.com",
@@ -14,17 +13,10 @@ var config = {
 firebase.initializeApp(config);
 
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
-
 const app = vertex.express();// initialize app
-
-const iframeReplacement = require('node-iframe-replacement');
-// add iframe replacement to express as middleware (adds res.merge method)
-app.use(iframeReplacement);
-
 // import routes
-const index = require('./routes/index')
+const index = require('./routes/index');
 // set routes
 app.use('/', index);
-
 
 module.exports = app;
--- diy-art/routes/index.js
+++ diy-art/routes/index.js
@@ -1,111 +1,66 @@
 // Full Documentation - https://www.turbo360.co/docs
-const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
-const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
-const router = vertex.router()
-var firebase = require("firebase");
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID});
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID});
+const router = vertex.router();
+const firebase = require("firebase");
 
-var database = firebase.database();
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('https://www.googleapis.com/auth/drive');
+const database = firebase.database();
 
-var flag = true;
-
-// const jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-// global.document = new JSDOM('localhost:3000/home').window.document;
-// var Blob = require('blob');
-//
-// var iframe = require('iframe')
-// frame = iframe({ container: document.querySelector('#iframe-cont') , scrollingDisabled: false})
-
-const products = {
-
-	car: {
-		title: 'Car',
-		image: '/images/turbo.png',
-		price: '50'
-	},
-
-	phone: {
-		title: 'Phone',
-		image: '/images/turbo.png',
-		price: '1000'
-	},
-
-	car1: {
-		title: 'Car',
-		image: '/images/turbo.png',
-		price: '50'
-	},
-
-	phone1: {
-		title: 'Phone',
-		image: '/images/turbo.png',
-		price: '1000'
-	},
-
-	car2: {
-		title: 'Car',
-		image: '/images/turbo.png',
-		price: '50'
-	},
-
-	phone2: {
-		title: 'Phone',
-		image: '/images/turbo.png',
-		price: '1000'
-	},
-
-	car3: {
-		title: 'Car',
-		image: '/images/turbo.png',
-		price: '50'
-	},
-
-	phone3: {
-		title: 'Phone',
-		image: '/images/turbo.png',
-		price: '1000'
-	}
-
-}
-
-/*  This is the home route. It renders the index.mustache page from the views directory.
-	Data is rendered using the Mustache templating engine. For more
-	information, view here: https://mustache.github.io/#demo */
 router.get('/', (req, res) => {
-	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
-})
+	res.render('DIY', null)
+});
 
 router.get('/home', (req, res) => {
-
 	res.render('DIY', null)
-})
+});
 
 router.get('/signup', (req, res) => {
 
 	const data = {
 	 type: 'Sign Up'
-	}
-  res.render('signin', data)
+	};
 
-})
+	res.render('signup', data)
+	/*firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			clicked1();
+		} else {
+
+		}
+	});*/
+
+});
+
+function clicked() {
+	alert("Added to cart successfully");
+}
+
+function clicked1() {
+	alert("User already signed in");
+}
 
 router.get('/signin', (req, res) => {
 
 	const data = {
 	 type: 'Sign In'
-	}
-  res.render('signin', data)
+	};
 
-})
+	res.render('signin', data)
+	/*firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			clicked1()
+		} else {
+
+		}
+	});*/
+
+});
 
 router.post('/login1', (req, res) => {
 
-  const body = req.body
-  const email = body.username
-  const password = body.pass
+  const body = req.body;
+  const email = body.username;
+  const password = body.pass;
 
   firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
     res.render('DIY', null)
@@ -118,13 +73,13 @@ router.post('/login1', (req, res) => {
   // ...
 });
 
-})
+});
 
 router.post('/login', (req, res) => {
 
-  const body = req.body
-  const email = body.username
-  const password = body.pass
+  const body = req.body;
+  const email = body.username;
+  const password = body.pass;
 
   firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
     res.render('DIY', null)
@@ -138,41 +93,132 @@ router.post('/login', (req, res) => {
   });
 
 
-})
+});
 
 router.post('/addProduct', (req,res) => {
-	const body = req.body
-	const prodId = ''+body.Name+'_'+body.Price
+	const body = req.body;
+	const prodId = ''+body.Name+'_'+body.Price;
 	const data = {
 		name: body.Name,
 		image: body.Image,
 		price: body.Price
-	}
+	};
 
 	database.ref('products/' + prodId).set(data);
 
   res.redirect('back');
-})
+});
+
+router.post('/add/:name/:price', (req,res) => {
+
+	const name = req.params.name;
+	const price = req.params.price;
+	const cartId = name+"_"+price;
+	const data = {
+		name: name,
+		price: price
+	};
+
+	database.ref('cart/' + cartId).set(data);
+	res.render('cart', null);
+});
+
+router.get('/getCartItems', (req, res) => {
+	const productRef = database.ref('cart/');
+
+	productRef.on('value', function(snapshot) {
+		const prod = snapshot.val();
+		const keys = Object.keys(prod);
+		const list = [];
+		keys.forEach(key => {
+			list.push(prod[key])
+		});
+
+		const data = {
+			products: list
+		};
+		//frame.setHTML({ body: 'Hello' })
+		res.render('cart', data)
+		// ...
+	});
+});
 
 router.get('/products', (req, res) => {
-  var productRef = database.ref('products/');
+	const productRef = database.ref('products/');
 
-  productRef.on('value', function(snapshot) {
+	productRef.on('value', function(snapshot) {
     const prod = snapshot.val();
-    const keys = Object.keys(prod)
-  	const list = []
+    const keys = Object.keys(prod);
+  	const list = [];
   	keys.forEach(key => {
   	list.push(prod[key])
-  	})
+  	});
 
   	 const data = {
   	 	products: list
-  	 }
+  	 };
      //frame.setHTML({ body: 'Hello' })
   	 res.render('products', data)
   // ...
 });
-})
+});
+
+
+router.get('/getProducts', (req, res) => {
+	const productRef = database.ref('products/');
+
+	productRef.on('value', function(snapshot) {
+		const prod = snapshot.val();
+		const keys = Object.keys(prod);
+		const list = [];
+		keys.forEach(key => {
+			list.push(prod[key])
+		});
+
+		const data = {
+			products: list
+		};
+		//frame.setHTML({ body: 'Hello' })
+		res.json({
+			confirmation: 'success',
+			app: process.env.TURBO_APP_ID,
+			data: data
+		})
+		// ...
+	});
+});
+
+router.get('/addNewProduct/:name/:price/:image', (req,res) => {
+
+	const name = req.params.name;
+	const price = req.params.price;
+	const image = req.params.image;
+	const prodId = ''+name+'_'+price;
+	const data = {
+		name: name,
+		image: image,
+		price: price
+	};
+
+	database.ref('products/' + prodId).set(data);
+
+	res.json({
+		confirmation: 'success',
+		app: process.env.TURBO_APP_ID
+	})
+});
+
+router.get('/removeCartItem/:id', (req,res) => {
+
+	const prodId = req.params.id+'';
+
+	database.ref('cart').child(prodId).remove();
+
+	res.json({
+		confirmation: 'success',
+		app: process.env.TURBO_APP_ID
+	})
+});
 
 /*  This route render json data */
 router.get('/json', (req, res) => {
@@ -181,22 +227,22 @@ router.get('/json', (req, res) => {
 		app: process.env.TURBO_APP_ID,
 		data: 'this is a sample json route.'
 	})
-})
+});
 
 
 router.get('/cart', (req, res) => {
 	res.render('cart',null)
-})
+});
 
 
 /*  This route sends text back as plain text. */
 router.get('/send', (req, res) => {
 	res.send('This is the Send Route')
-})
+});
 
 /*  This route redirects requests to Turbo360. */
 router.get('/redirect', (req, res) => {
 	res.redirect('https://www.turbo360.co/landing')
-})
+});
 
-module.exports = router
+module.exports = router;
