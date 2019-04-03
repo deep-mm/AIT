83a6969b7c8343afda831c7f379a812ff2269b92
Deep Mehta <deep.mm@somaiya.edu>
2019-04-03 10:40:06 +0530

    Update

---
+++ angular-js/aitExp5/src/app/CustomPipe1.ts
@@ -0,0 +1,3 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'custom-pipe1'})
---
+++ angular-js/aitExp5/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'aitExp5';
+  todoArray = [];
+
+  addTodo(value){
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+}
